d53585595d181e3dc2f1a6858b0d2381ccded01f
Letsdoit1 <itisfunletsdoit@gmail.com>
2022-07-29 07:34:15 +0530

    Updating LinkedList file.

--- AlgoPractice_C++_CLI/LinkedList.h
+++ AlgoPractice_C++_CLI/LinkedList.h
@@ -2,11 +2,21 @@
 #define LINKEDLIST_H
 #include <iostream>
 
+class Node {
+public:
+	int intValue; string strValue;
+	Node* next; Node* previous;
+
+	Node() {
+	}
+	Node(int v, int s) {
+		intValue = v; strValue = s;
+	}
+};
 
 class LinkedList {
 
 };
-
 
 int linkedList_main() {
 	
